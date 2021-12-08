843170a93b97b0d57dfd5a7ab857a4f277a9123d
Sh6lok <95735865+Sh6lok@users.noreply.github.com>
2021-12-08 12:35:08 +0500

    site

--- index.php
+++ index.php
@@ -4,97 +4,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Comfortaa&family=Jura&display=swap" rel="stylesheet">
-    
     <title>Портфолио работ</title>
 
 </head>
 <body>
-<header class="intro">
-
-<div class="header">
-    <a href="http://localhost" class="logo" >Shalok</a>
-    <a href="http://localhost/email/email.php" class="nav-link">Отправить почту</a>
-    <a href="http://localhost/Market/" class="nav-link">Маркет</a>
-    <a href="http://localhost/weather/" class="nav-link">Погода</a>
-    <a href="http://localhost/authorization/" class="nav-link">Авторизация</a>
-
+<div class="intro">
+    <div class="container">
+        <header>
+            <img src="img/logo.png" class="logo">
+        </header>
+    </div>
 </div>
-        
-<div class="intro-inner">
-    <h1 class="intro-header">Портфолио работ по web дезайну с использованием html, css, php, mysql</h1>
-</div>
-
-</div>
-
-<div class="time">
-<p style="font-family: 'Comfortaa', cursive;
-            color:white;
-            display: flex;
-            justify-content: center;
-            text-align: center;
-            font-size: 4.2vh;
-            
-            ">
-    <?php $chelyabinsk=date_default_timezone_set('Asia/Yekaterinburg');$chelyabinsk = date("H:i");
-    echo "Сейчас в Челябинске <br>$chelyabinsk";?>
-</p>  
-</header>
-
-<h1 style=
-"font-size: 9vh;
-text-align: center;
-padding: 7vh;
-font-family: 'Jura', sans-serif;
-background-color: #F0F0F0;"
->Портфолио работ</h1>
-
-<div class="main">
-    <a class="Block" href="http://localhost/email/email.php">
-
-
-        <img src="img/email.jpg" class="block_photo">
-        <h1 style="
-        font-family: 'Jura', sans-serif; 
-        font-size: 3vh; text-align:center; 
-        
-        padding-bottom:1vh;
-        ">Отправка писем на Email</h1>
-        <hr style="width: 7vh; ">
-        <h2 style="font-family: 'Comfortaa', cursive; font-size:2vh; padding-top:1vh;"> Используется для обратной связи. </h2>
-
-        
-    </a>
-
-    <a class="Block" href="http://localhost/weather/">
-        <img src="img/weather.jpg" class="block_photo">
-        <h1 style="
-        font-family: 'Jura', sans-serif; 
-        font-size: 3vh; text-align:center; 
-        
-        padding-bottom:1vh;
-        ">Прогноз погоды</h1>
-        <hr style="width: 7vh; ">
-        <h2 style="font-family: 'Comfortaa', cursive; font-size:2vh; padding-top:1vh;"> Используется для получения погодных условий. </h2>
-    </a>
-
-    <a class="Block" href="http://localhost/Market/">
-        <img src="img/market.jpg" class="block_photo">
-        <h1 style="
-        font-family: 'Jura', sans-serif; 
-        font-size: 3vh; text-align:center; 
-        
-        padding-bottom:1vh;
-        ">Маркет</h1>
-        <hr style="width: 7vh; ">
-        <h2 style="font-family: 'Comfortaa', cursive; font-size:2vh; padding-top:1vh;"> Используется для создания, удаления, просмотра и удаления позиций. </h2>
-    </a>
-
-</div>
-
-<div style="padding-top: 100vh; background-color: #F0F0F0;">s</div>
 
 </body>
 </html>
